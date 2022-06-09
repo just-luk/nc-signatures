38007149fb87b6dd84ce03eec8e7e8672da89f1d
Luke Griffith <lukegriffith@gmail.com>
2022-06-09 12:31:07 +0000

    add gitpod setup

--- kodo/build/resolve_symlinks/gtest-source/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/just-luk.github.io/kodo/resolve_symlinks/gtest-source
-# Build directory: /workspace/just-luk.github.io/kodo/build/resolve_symlinks/gtest-source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- kodo/build/resolve_symlinks/stub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/just-luk.github.io/kodo/resolve_symlinks/stub
-# Build directory: /workspace/just-luk.github.io/kodo/build/resolve_symlinks/stub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
