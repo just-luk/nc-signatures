088f8c74ad9d2ba9e19011de5af0af585b5ef4c8
justluk <lukegriffith@gmail.com>
2022-07-18 18:54:00 -0400

    fix bug

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 rm -rf ~/.local/include/kodr
 rm -rf ~/.local/lib/libkodr.a
 cp -r kodr/include ~/.local/include/kodr
-cp -r kodr/build/src/libkodr.a ~/.local/lib/libkodr.a
+cp -r build/kodr/src/libkodr.a ~/.local/lib/libkodr.a
 echo "kodr installed"
 
